48c39af09a7aa5c1195ae49969165f7b853d8fc9
Vlad Levitin <levitinvlad@hotmail.com>
2022-09-27 01:59:34 +0200

    new commit

--- Assignment 2/exercise2-files/part2.py
+++ Assignment 2/exercise2-files/part2.py
@@ -16,6 +16,9 @@ np.set_printoptions(threshold=sys.maxsize)
 class SQLQueries:
     def __init__(self) -> None:
         
+        #(showing top 10 rows from all of your tables is sufficient)
+        self.part1 = {table_name: f'SELECT * FROM {table_name} LIMIT 10;' for table_name in ['User', 'Activity', 'TrackPoint']}
+
         # How many users, activities and trackpoints are there in the dataset (after it is inserted into the database).
         self.task1 = {table_name: f'SELECT COUNT(*) FROM {table_name};' for table_name in ['User', 'Activity', 'TrackPoint']}
         
@@ -89,7 +92,7 @@ class SQLQueries:
         # Do not count rows where mode is null
         self.task11 = """SELECT user_id, transportation_mode FROM Activity WHERE transportation_mode IS NOT NULL;"""
         self.task11alt = """SELECT user_id, transportation_mode, COUNT(*) FROM Activity GROUP BY user_id WHERE transportation_mode IS NOT NULL HAVING COUNT(*) >= ALL (SELECT COUNT(*) FROM Activity GROUP BY user_id);"""
-
+        
 class Program:
         def __init__(self):
             self.connection = DbConnector()
@@ -198,69 +201,76 @@ def main():
     all_queries = SQLQueries()
     
     try:
-            # print("\nTask 2.1:\n")
-            # for table_name, query in all_queries.task1.items():
-            #     print(f"Table: {table_name}\n")
-            #     program.fetch_data(query, table_name)
+            print("\nTask 1:\n")
+            for table_name, query in all_queries.part1.items():
+                print(f"Table: {table_name}\n")
+                program.fetch_data(query, table_name)
+            
+            
+            print("\nTask 2.1:\n")
+            for table_name, query in all_queries.task1.items():
+                print(f"Table: {table_name}\n")
+                program.fetch_data(query, table_name)
                 
 
-            # print("\nTask 2.2:")
-            # print("Average number of activties per user:\n")
-            # program.cursor.execute(all_queries.task2_init)
-            # program.fetch_data(all_queries.task2)
+            print("\nTask 2.2:")
+            print("Average number of activties per user:\n")
+            program.cursor.execute(all_queries.task2_init)
+            program.fetch_data(all_queries.task2)
             
     
-            # print("\nTask 2.3:")
-            # print("Top 20 users with most activities:\n")
-            # program.fetch_data(all_queries.task3)
+            print("\nTask 2.3:")
+            print("Top 20 users with most activities:\n")
+            program.fetch_data(all_queries.task3)
             
-            # print("\nTask 2.4:")
-            # print("Users who have taken a taxi:\n")
-            # program.fetch_data(all_queries.task4)
+            print("\nTask 2.4:")
+            print("Users who have taken a taxi:\n")
+            program.fetch_data(all_queries.task4)
             
-            #print("\nTask 2.5:")
-            #print("Types of transportation modes and their activities count:\n")
-            #program.fetch_data(all_queries.task5)
+            print("\nTask 2.5:")
+            print("Types of transportation modes and their activities count:\n")
+            program.fetch_data(all_queries.task5)
             
-            #print("\nTask 2.6a:")
-            #print("The year with the most activities:\n")
-            #program.fetch_data(all_queries.task6a)
+            print("\nTask 2.6a:")
+            print("The year with the most activities:\n")
+            program.fetch_data(all_queries.task6a)
 
-            #print("\nTask 2.6b:")
-            #print("The year with the most recorded hours:\n")
-            #program.fetch_data(all_queries.task6b)
+            print("\nTask 2.6b:")
+            print("The year with the most recorded hours:\n")
+            program.fetch_data(all_queries.task6b)
 
-            #print("\nTask 2.7:")
-            #print("Total distance walked in 2008 by user with id=112:\n")
-            #track_points, columns = program.fetch_data_with_columns(all_queries.task7, print_results=False)
-            #track_points = pd.DataFrame(track_points, columns=columns)
-            #print(program.odometer(track_points))
+            print("\nTask 2.7:")
+            print("Total distance walked in 2008 by user with id=112:\n")
+            track_points, columns = program.fetch_data_with_columns(all_queries.task7, print_results=False)
+            track_points = pd.DataFrame(track_points, columns=columns)
+            print(program.odometer(track_points))
 
-            #print("\nTask 2.8:")
-            #print("Top 20 users who have gained the most altitude:\n")
-            #track_points, columns = program.fetch_data_with_columns(all_queries.task8, print_results=False)
-            #track_points = pd.DataFrame(track_points, columns=columns)
-            #altitude_df = program.altitude_odometer(track_points)
-            #print(altitude_df.nlargest(20, 'altitude_gained'))
+            print("\nTask 2.8:")
+            print("Top 20 users who have gained the most altitude:\n")
+            track_points, columns = program.fetch_data_with_columns(all_queries.task8, print_results=False)
+            track_points = pd.DataFrame(track_points, columns=columns)
+            altitude_df = program.altitude_odometer(track_points)
+            print(altitude_df.nlargest(20, 'altitude_gained'))
 
-            #print("\nTask 2.9:")
-            #print("Invalid activities per user:\n")
-            #date_points, columns = program.fetch_data_with_columns(all_queries.task9, print_results=False)
-            #date_points = pd.DataFrame(date_points, columns=columns)
-            #invalid_df = program.invalid_activities(date_points)
-            #pd.set_option('display.max_rows', None)
-            #print(invalid_df)
+            print("\nTask 2.9:")
+            print("Invalid activities per user:\n")
+            date_points, columns = program.fetch_data_with_columns(all_queries.task9, print_results=False)
+            date_points = pd.DataFrame(date_points, columns=columns)
+            invalid_df = program.invalid_activities(date_points)
+            pd.set_option('display.max_rows', None)
+            print(invalid_df)
             
             print("\nTask 2.10:")
             print("Users who have tracked an activity in the Forbidden City of Beijing:\n")
             program.fetch_data(all_queries.task10)
             
+            print("\nTask 2.11:")
+            print("Most used transportation mode for all users:")
+            trans_modes, columns = program.fetch_data_with_columns(all_queries.task11, print_results=False)
+            trans_modes = pd.DataFrame(trans_modes, columns=columns)
+            print(program.most_used_transportation(trans_modes))
             
-            # print("\nTask 2.11:")
-            # print("Most used transportation mode for all users:")
-            # trans_modes, columns = program.fetch_data_with_columns(all_queries.task11, print_results=False)
-            # trans_modes = pd.DataFrame(trans_modes, columns=columns)
-            # print(program.most_used_transportation(trans_modes))
+            
 
     except Exception as e:
         print("ERROR: Failed to use database:", e)
