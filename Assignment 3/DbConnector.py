f607fbee6746c22d7bfb17fb5d75dde6a8915130
Vlad Levitin <levitinvlad@hotmail.com>
2022-10-11 17:52:37 +0200

    new commit

--- Assignment 3/DbConnector.py
+++ Assignment 3/DbConnector.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient, version
-
+import os
 
 class DbConnector:
     """
@@ -13,10 +13,10 @@ class DbConnector:
     """
 
     def __init__(self,
-                 DATABASE='DATABASE_NAME',
-                 HOST="tdt4225-xx.idi.ntnu.no",
-                 USER="TEST_USER",
-                 PASSWORD="test123"):
+                 DATABASE='my_db',
+                 HOST="tdt4225-37.idi.ntnu.no",
+                 USER="user",
+                 PASSWORD=os.environ.get('MONGODB_PASSWORD')):
         uri = "mongodb://%s:%s@%s/%s" % (USER, PASSWORD, HOST, DATABASE)
         # Connect to the databases
         try:
