5b9ddddcf5cd5cd079b227d1d0d235872d1c22a6
kritveit <kritveit@stud.ntnu.no>
2022-10-15 12:06:01 +0200

    Task 2 and 3 done

--- Assignment 3/part2.py
+++ Assignment 3/part2.py
@@ -37,16 +37,56 @@ class Part2Program:
         print("{" + f"'activities':{amount_activities}" + "}")
         self.show_result(amount_trackpoints)
 
-    
+    def part_2(self):
 
-        
+        activity_average = self.db.user.aggregate([
+            {"$group": {"_id": None,
+                        "Average_activities": {"$avg": {"$size": "$activity_refs"}}}},
+            {"$project": {"_id": 0, "Average_activities": {"$round": ["$Average_activities", 3]}}}
+        ])
+
+        self.show_result(activity_average)
+
+
+    def part_3(self):
+        """As the users are allready in a collection, the refs can just be summes up per user."""
+
+        activity_average = self.db.user.aggregate([
+            {"$project": {"_id": 1, "Total_activities": {"$size": "$activity_refs"}}},
+            {"$sort": {"Total_activities": -1}},
+            {"$limit": 20},
+        ])
+
+        self.show_result(activity_average)
+
+
+    def part_4(self):
+        """Find all users that have taken a taxi"""
+
+
+
+
+
 def main():
     program = None
     try:
         program = Part2Program()
 
         print("Part 2.1:", sep="\n")
+        print("Total amount of users, activities and trackpoints in the dataset")
         program.part_1()
+
+        print("\nPart 2.2:", sep="\n")
+        print("Find the average number of activities per user")
+        program.part_2()
+
+        print("\nPart 2.3:", sep="\n")
+        print("Find top 20 users with the highest number of activities")
+        program.part_3()
+
+        print("\nPart 2.4:", sep="\n")
+        print("Find all users who have taken a taxi")
+        program.part_4()
 
 
     except Exception as e:
