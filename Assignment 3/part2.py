7ff497a25aba082a79c7976e53106693660d8b22
kritveit <kritveit@stud.ntnu.no>
2022-10-15 23:59:36 +0200

    Task 4 and 5 finished. Task 6a begun, struggelig with converting ISO date from string

--- Assignment 3/part2.py
+++ Assignment 3/part2.py
@@ -49,22 +49,70 @@ class Part2Program:
 
 
     def part_3(self):
-        """As the users are allready in a collection, the refs can just be summes up per user."""
+        """
+        Find the top 20 users with the most activities.
+        As the users are allready in a collection, the refs can just be summes up per user."""
 
-        activity_average = self.db.user.aggregate([
+        most_activities = self.db.user.aggregate([
             {"$project": {"_id": 1, "Total_activities": {"$size": "$activity_refs"}}},
             {"$sort": {"Total_activities": -1}},
             {"$limit": 20},
         ])
 
-        self.show_result(activity_average)
+        self.show_result(most_activities)
 
 
     def part_4(self):
         """Find all users that have taken a taxi"""
+        """
+        Lookup is for matching entries based on fields
+        Unwind is for creating a new duplicate row per array element in a single row
+        
+        """
 
+        taken_taxi = self.db.user.aggregate([
+            # Unwind the activity refs into their own row
+            {"$unwind": {"path": "$activity_refs"}},
+            # Join activity collection:
+            {"$lookup": {"from": "activity",
+                         "localField": "activity_refs",
+                         "foreignField": "_id",
+                         "as": "activity"}},
+            {"$project": {"_id": 1, "activity.transportation_mode": 1}},
+            {"$match": {"activity.transportation_mode": "taxi"}},
+            {"$group": {"_id": "$_id"}},
+            {"$sort": {"_id": 1}}
+        ])
+        self.show_result(taken_taxi)
 
+    def part_5(self):
+        """
+        Find all types of transportation modes and count how many activities
+        that are tagged with each transportation mode
+        Null rows are ignored
+        """
+        transportation_mode_activities = self.db.activity.aggregate([
+            {"$match": {"transportation_mode": {"$exists": "true", "$ne": None}}},
+            {"$group": {"_id": "$transportation_mode", "activities": {"$count": {}}}},
+            {"$sort": {"activities": 1}}
+        ])
 
+        self.show_result(transportation_mode_activities)
+
+    def part_6a(self):
+        """
+        Find the year with the most activities.
+        """
+        year_with_most_activities = self.db.activity.aggregate([
+            {"$group":
+                 {"_id":
+                    {"$toInt":
+                       {"$year": datetime.datetime("$start_date_time")}
+                    }
+                }
+             }
+        ])
+        self.show_result(year_with_most_activities)
 
 
 def main():
@@ -72,21 +120,29 @@ def main():
     try:
         program = Part2Program()
 
-        print("Part 2.1:", sep="\n")
-        print("Total amount of users, activities and trackpoints in the dataset")
-        program.part_1()
+        #print("Part 2.1:", sep="\n")
+        #print("Total amount of users, activities and trackpoints in the dataset")
+        #program.part_1()
 
-        print("\nPart 2.2:", sep="\n")
-        print("Find the average number of activities per user")
-        program.part_2()
+        #print("\nPart 2.2:", sep="\n")
+        #print("Find the average number of activities per user")
+        #program.part_2()
 
-        print("\nPart 2.3:", sep="\n")
-        print("Find top 20 users with the highest number of activities")
-        program.part_3()
+        #print("\nPart 2.3:", sep="\n")
+        #print("Find top 20 users with the highest number of activities")
+        #program.part_3()
 
-        print("\nPart 2.4:", sep="\n")
-        print("Find all users who have taken a taxi")
-        program.part_4()
+        #print("\nPart 2.4:", sep="\n")
+        #print("Find all users who have taken a taxi")
+        #program.part_4()
+
+        #print("\nPart 2.5:", sep="\n")
+        #print("Find activities per transportation mode")
+        #program.part_5()
+
+        print("\nPart 2.6a:", sep="\n")
+        print("Find the year with the most activities")
+        program.part_6a()
 
 
     except Exception as e:
